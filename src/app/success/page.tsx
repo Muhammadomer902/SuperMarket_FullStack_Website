bd23799263dfe16d5da92daf54217803479050a1
Muhammad omer <sayyidmuhammadomer@gmail.com>
2025-01-26 18:56:22 +0500

    Fixedstuff18

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,38 +1,47 @@
 "use client";
+
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const SuccessPage = () => {
+  const [isClient, setIsClient] = useState(false);
   const router = useRouter();
   const searchParams = useSearchParams();
   const payment_intent = searchParams.get("payment_intent");
 
   useEffect(() => {
-    const makeRequest = async () => {
-      try {
-        await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, {
-          method: "PUT",
-        });
-        setTimeout(() => {
-          router.push("/orders");
-        }, 5000);
-      } catch (err) {
-        console.log(err);
-      }
-    };
+    setIsClient(true); // Runs only on the client side
+  }, []);
 
-    makeRequest();
-  }, [payment_intent, router]);
+  useEffect(() => {
+    if (payment_intent && isClient) {
+      const makeRequest = async () => {
+        try {
+          await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, {
+            method: "PUT",
+          });
+          setTimeout(() => {
+            router.push("/orders");
+          }, 5000);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      makeRequest();
+    }
+  }, [payment_intent, router, isClient]);
+
+  if (!isClient) {
+    return <p>Loading ... </p>; // Prevent render during SSR
+  }
 
   return (
-    <>
-      <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
-        <p className="max-w-[600px]">
-          Payment successful. You are being redirected to the orders page.
-          Please do not close the page.
-        </p>
-      </div>
-    </>
+    <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
+      <p className="max-w-[600px]">
+        Payment successful. You are being redirected to the orders page. Please do not close the page.
+      </p>
+    </div>
   );
 };
 
