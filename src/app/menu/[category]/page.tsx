90832d598aca39ee90a64379c0ddf1a64db460e2
Muhammad omer <sayyidmuhammadomer@gmail.com>
2025-01-26 17:00:35 +0500

    Fixedstuff9

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -4,51 +4,43 @@ import Image from "next/image";
 import { ProductType } from "@/types/types";
 
 //res = result
-const getProducts = async (category:string) => {
-    const res = await fetch(`http://localhost:3000/api/products?cat=${category}`, {cache:"no-store"} );
-  
-    if(!res.ok){
-      throw new Error("Failed!");
-    }
-  
-    return res.json()
-}
+const getProducts = async (category: string) => {
+  const res = await fetch(`http://localhost:3000/api/products?cat=${category}`, { cache: "no-store" });
 
-type Props = {
-    params: {category:string}
-}
+  if (!res.ok) {
+    throw new Error("Failed!");
+  }
 
-const CategoryPage = async ({params}:Props) => {
+  return res.json();
+};
 
-    const products: ProductType[] = await getProducts(params.category);
+const CategoryPage = async ({ params }: { params: { category: string } }) => {
+  const products: ProductType[] = await getProducts(params.category);
 
-    return (
-        <div className="flex flex-wrap text-sky-800">
-            {
-                products.map(item=>(
-                    <Link className="w-full h-[60vh] border-l-2 border-t-2 border-r-2 border-b-2 border-sky-600 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group hover:bg-fuchsia-200 even:bg-fuchsia-100" href={`/product/${item.id}`} key={item.id}>
-                        {/* Image Container */}
-                        {
-                            item.img && 
-                            (
-                                <div className="relative h-[80%]">
-                                    <Image src={item.img} alt="" fill className="object-contain"/>
-                                </div>
-                            )
-                        }
-                        {/* Text Container */}
-                        {
-                            <div className="flex items-center justify-between font-bold group">
-                                <h1 className="text-2xl uppercase p-2">{item.title}</h1>
-                                <h2 className="group-hover:hidden text-xl">${item.price}</h2>
-                                <button className="hidden group-hover:block bg-sky-800 text-white uppercase p-2 rounded-md">ADD TO CART</button>
-                            </div>
-                        }
-                    </Link>
-                ))
-            }
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-wrap text-sky-800">
+      {products.map((item) => (
+        <Link
+          className="w-full h-[60vh] border-l-2 border-t-2 border-r-2 border-b-2 border-sky-600 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group hover:bg-fuchsia-200 even:bg-fuchsia-100"
+          href={`/product/${item.id}`}
+          key={item.id}
+        >
+          {/* Image Container */}
+          {item.img && (
+            <div className="relative h-[80%]">
+              <Image src={item.img} alt="" fill className="object-contain" />
+            </div>
+          )}
+          {/* Text Container */}
+          <div className="flex items-center justify-between font-bold group">
+            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+            <button className="hidden group-hover:block bg-sky-800 text-white uppercase p-2 rounded-md">ADD TO CART</button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default CategoryPage;
