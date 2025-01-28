0c2dbbb07399d5fda2d43df888e906d4f8c708ba
Muhammad omer <sayyidmuhammadomer@gmail.com>
2025-01-28 11:26:29 +0500

    FixedStuff23

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -80,7 +80,7 @@ const AddPage = () => {
         try {
             const url = await upload();
             
-          const res = await fetch("http://localhost:3000/api/products", {
+          const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`, {
             method: "POST",
             body: JSON.stringify({
               img:url,
